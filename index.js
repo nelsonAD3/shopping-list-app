6610d6cfe4b28c737e209743b7f2c4638e55be2f
Nelson AD <ndemoraes93@gmail.com>
2019-03-26 16:47:42 -0400

    added index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+
+
+function AddEntry() {
+    $('#js-shopping-list-form').submit(event => {
+      // this stops the default form submission behavior
+      event.preventDefault();
+      const entry = $('#shopping-list-entry').val();
+      
+      // $(event.currentTarget).find('#number-choice');
+      // $('input:number').val();
+      console.log(entry);
+      $('.shopping-list').append(
+        "<li><span class='shopping-item'>"+entry+"</span><div class='shopping-item-controls'><button class='shopping-item-toggle'><span class='button-label'>check</span></button><button class='shopping-item-delete'><span class='button-label'>delete</span></button></div></li>"
+        )
+        $('.shopping-item-toggle').on( "click", function() {
+          
+          $(this).parent("div").prev().addClass('shopping-item__checked');
+          
+           });
+      })
+  
+  }
+
+function CheckEntry()  { 
+    // $('.shopping-item-toggle').submit(event => {
+    //   console.log('got here');
+    //   // event.preventDefault();
+      
+    //   $(this).addClass('.shopping-item__checked');
+    
+    $('.shopping-item-toggle').on( "click", function() {
+      
+      $(this).parent("div").prev().addClass('shopping-item__checked');
+      
+       }); 
+      }
+
+function DeleteEntry(){
+
+  $('.shopping-item-delete').on( "click", function() {
+    console.log( "click" );  
+    $(this).parent().parent().remove();
+    
+     }); 
+    }
+
+
+
+
+        
+//     })
+  
+// }
+
+
+
+
+CheckEntry();
+AddEntry();
+DeleteEntry();
